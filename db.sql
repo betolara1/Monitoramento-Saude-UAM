9285f3c2745c7e977861295ad87dc9d575e5728c
Roberto <robertokantovitzlara@hotmail.com>
2025-02-17 10:36:53 -0300

    atualização

--- db.sql
+++ db.sql
@@ -163,3 +163,15 @@ CREATE TABLE IF NOT EXISTS riscos_saude (
     pontuacao INT NOT NULL,
     FOREIGN KEY (paciente_id) REFERENCES pacientes(id) ON DELETE CASCADE
 );
+
+
+CREATE TABLE IF NOT EXISTS notificacoes_medicamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    medicamento_id INT NOT NULL,
+    paciente_id INT NOT NULL,
+    horario_previsto DATETIME NOT NULL,
+    notificacao_enviada BOOLEAN DEFAULT FALSE,
+    data_envio DATETIME,
+    FOREIGN KEY (medicamento_id) REFERENCES medicamentos(id) ON DELETE CASCADE,
+    FOREIGN KEY (paciente_id) REFERENCES pacientes(id) ON DELETE CASCADE
+);
